779022a584aa185e2b3e9147f94ec928268cdd04
jwhoster <98hiro35@gmail.com>
2021-11-16 09:49:35 +0900

    コミット練習

---
+++ inc/head.php
@@ -0,0 +1,26 @@
+<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="format-detection" content="telephone=no">
+		<title><?php echo PAGE_TITLE; ?></title>
+		<meta property="og:title" content="<?php echo PAGE_TITLE; ?>">
+		<meta name="author" content="<?php echo SITE_NAME; ?>">
+		<meta name="description" content="<?php echo DESCRIPTION; ?>">
+		<meta name="keywords" content="<?php echo KEYWORDS; ?>">
+		<meta property="og:url" content="<?php echo $pageUrl; ?>">
+		<meta property="og:type" content="<?php echo OGTYPE; ?>">
+		<meta property="og:site_name" content="<?php echo SITE_NAME; ?>">
+		<meta property="og:image" content="<?php echo DOCUMENT_URL; ?>favicon/ogp_image.jpg">
+		<meta property="og:description" content="<?php echo DESCRIPTION; ?>">
+		<link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png">
+		<link rel="manifest" href="/images/favicon/site.webmanifest">
+		<link rel="mask-icon" href="/images/favicon/safari-pinned-tab.svg" color="#0151ac">
+		<meta name="msapplication-TileColor" content="#0151ac">
+		<meta name="theme-color" content="#ffffff">
+		<link rel="stylesheet" type="text/css" href="/css/styles.css">
+		<link rel="canonical" href="<?php echo $pageUrl; ?>">
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree:ital,wght@0,500;0,600;1,500;1,600&family=Noto+Sans+JP:wght@100;300;400;500;700;900&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap" rel="stylesheet">
---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+ 
+    define("PAGE_TITLE", "株式会社");
+    define("DESCRIPTION", "テキスト");
+    define("KEYWORDS", "上場企業");
+    define("OGTYPE", "website");
+ 
+?>
+<!DOCTYPE html>
+<html lang="ja" id="pagetop">
+    <head prefix="og: https://ogp.me/ns#">
+        <?php include("inc/head.php"); ?>
+        <link type="text/css" rel="stylesheet" href="/css/page/index.css">
+    </head>
+    
+    <body>
+        <header id="header">
+            <div id="header-logo">
+                <img src="/images/index/logo.svg" alt="">
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="dead"><span>COMPANY</span>会社概要</a></li>
+                    <li><a href="dead"><span>SERVICE</span>事業内容</a></li>
+                    <li><a href="dead"><span>NEWS</span>お知らせ</a></li>
+                    <li><a href="dead"><span>CONTACT</span>お問い合わせ</a></li>
+                </ul>
+            </nav>
+        </header>
+
+        <div class="key-visual">
+            <!-- <img src="/images/index/key_visual.jpg" alt=""> -->
+        </div>
+
+        <div id="contact">
+            <header class="title01">
+                <h3>CONTACT</h3>
+                <p>お問合せ</p>
+            </header>
+            <p class="text">お見積もり・ご相談など、お気軽にお問い合わせください。</p>
+            <div class="contact-info">
+                <p class="tel"><span>お電話でのお問合せ</span><img src="/images/index/icon_tel.svg" alt="">052-123-4567</p>
+                <p class="mail"><img src="/images/index/icon_mail.svg" alt="">メールお問合せ</p>
+            </div>
+        </div>
+
+        <footer id="footer">
+            
+
+        </footer>
+            
+        
+    </body>
+</html>
